d8b2c76fce5043381de54d8066f56a6778575aa9
meixichen <meixichen2010@gmail.com>
2021-12-03 12:07:45 -0500

    Update get_FA_estim
    Instead of extracting parameter estimates by indices, now extract them by names.

--- R/get_FA_estim.R
+++ R/get_FA_estim.R
@@ -8,13 +8,14 @@
 
 get_FA_estim <- function(mod_fit, n_cell, n_factor){
   estimates <- mod_fit$par
-  off_diag_L_est <- round(estimates[(2*n_cell+1):length(estimates)],3)
+  off_diag_L_est <- estimates[names(estimates)=="Lt"]
   L_est <- matrix(0, nrow=n_cell, ncol=n_factor)
   L_est[lower.tri(L_est, diag=TRUE)] <- off_diag_L_est
   psi <- rep(1, n_cell)
   res <- norm_mat(L_est, psi, 3)
   
-  fi <- unname(exp(estimates[(n_cell+1):(2*n_cell)]-estimates[1:n_cell]))
+  fi <- unname(exp(estimates[names(estimates)=="log_a"]-
+                     estimates[names(estimates)=="log_k"]))
   return(list(
     L=res$L,
     fi=round(fi,3)
