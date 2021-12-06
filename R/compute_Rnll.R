b35373fd774cecf188fc791cb66b5d30d6e91f75
meixichen <meixichen2010@gmail.com>
2021-12-06 11:22:59 -0500

    Add unit test for factor_model.hpp

---
+++ R/compute_Rnll.R
@@ -0,0 +1,59 @@
+#' Compute the negative log-likelihood of the factor model in R
+#' @param data A list of a 3d data array `Y`, penalization parameter `lam`, and num of factors `n_factor`.
+#' @param param_list A list of parameters including vector `log_k`, vector `log_a`, 3d array `x` (latent paths), 
+#' and vector `Lt` (unnormalized lower tri elements of loading matrix)
+#' @return Scalar of negative log-likelihood
+#' @export
+
+compute_Rnll <- function(data, param_list){
+  k <- exp(param_list$log_k)
+  alpha <- exp(param_list$log_a)
+  Lt <- param_list$Lt
+  x <- param_list$x
+  dim_x <- dim(x)
+  n_cell <- dim_x[1]
+  n_bin <- dim_x[2]
+  n_trial <- dim_x[3]
+  n_factor <- data$n_factor
+  Y <- data$Y
+  lam <- data$lam
+  #----- fill in the covariance matrix for latent increments -----
+  L <- matrix(0, nrow=n_cell, ncol=n_factor) # loading matrix
+  psi <- rep(0, n_cell) # diagonal of uniqueness matrix
+  indx <- 0
+  for (j in 1:n_factor){ # column
+    for (i in j:n_cell){ # row
+      indx <- indx + 1
+      L[i,j] <- Lt[indx]
+    }
+  }
+  for (i in 1:n_cell){ # row
+    norm2 <- 1
+    for (j in 1:n_factor){
+      norm2 <- norm2 + L[i,j]^2
+    }
+    psi[i] <- 1/norm2
+    for (j in 1:n_factor){
+      L[i,j] <- L[i,j]/sqrt(norm2)
+    }
+  }
+  Sig <- L%*%t(L) + diag(psi)
+  #----- compute nll -----------
+  nll <- lam*sum(Lt^2) # l2 penalization term
+  for (u in 1:n_trial){
+    mu <- x[, 1, u] - alpha*dt
+    nll <- nll - mvtnorm::dmvnorm(mu, sigma=Sig*dt, log=T)
+    for (w in 2:n_bin){
+      mu <- x[, w, u] - x[, w-1, u] - alpha*dt
+      nll <- nll - mvtnorm::dmvnorm(mu, sigma=Sig*dt, log=T)
+    }
+    Nt <- rep(1, n_cell)
+    for (j in 1:n_bin){
+      for (i in 1:n_cell){
+        nll <- nll - dbinom(Y[i, j, u], 1, invlogit(x[i, j, u] - Nt[i] * k[i]), log=T)
+        Nt[i] <- Nt[i] + Y[i, j, u] 
+      }
+    }
+  }
+  return(nll)
+}
---
+++ tests/testthat/test-factor_model.R
@@ -0,0 +1,66 @@
+context("factor_model")
+
+test_that("Factor model in C++ has same nll as the nll computed in R. ",{
+  n_test <- 10
+  for (ii in 1:n_test){
+    #---- Data simulation ---------
+    dt <- 0.005
+    n_bin <- 2000
+    n_cell <- sample(2:10, 1)
+    k <- runif(n_cell, 0.1, 0.4)
+    alpha <- runif(n_cell, 2, 5)
+    n_trial <- 5
+    Sig <- cov2cor(rWishart(1, n_cell+1, diag(n_cell))[,,1])
+    rho <- Sig[lower.tri(Sig)]
+    sim <- simdata(dt=dt, n_bin=n_bin, n_trial=n_trial, alpha=alpha, k=k, rho=rho)
+    Y <- sim$Y
+    x <- sim$x
+    
+    #---- TMB model nll -----------
+    n_factor <- ifelse(n_cell >= 5, 2, 1)
+    init_param <- list(log_k = rep(-2, n_cell),
+                       log_a = rep(0, n_cell),
+                       Lt = rep(1, n_cell*n_factor-n_factor*(n_factor-1)/2),
+                       x = prop_paths(Y, dt, rep(-2, n_cell), rep(0, n_cell)))
+    data <- list(model="factor_model", n_factor=n_factor, dt=dt, Y=Y, lam=1)
+    adfun <- TMB::MakeADFun(data=data,
+                            parameters=init_param,
+                            DLL = "mnfa_TMBExports", 
+                            silent = TRUE)
+    nll_tmb <- adfun$fn(unlist(init_param)) # negative log-likelihood computed by TMB/C++
+    
+    #---- R nll --------------
+    nll_r <- compute_Rnll(data, init_param)
+    expect_equal(nll_tmb, nll_r)
+  }
+})
+
+test_that("Optimization of the factor model converges and gives PD Hessian.",{
+  dt <- 0.005
+  n_bin <- 2000
+  k <- c(0.15, 0.3, 0.2, 0.27, 0.25)
+  alpha <- c(2.5, 4, 3.5, 5, 3)
+  n_cell <- length(alpha)
+  n_trial <- 5
+  l1 <- c(0.8, 0.08, 0.75, 0.1, 0.8)
+  l2 <- c(0, 0.8, 0.05, 0.85, 0.1)
+  L <- cbind(l1, l2)
+  sim <- simdata(dt=dt, n_bin=n_bin, n_trial=n_trial, alpha=alpha, k=k, L=L)
+  Y <- sim$Y
+  x <- sim$x
+  n_factor <- 2
+  init_param <- list(log_k = rep(-2, n_cell),
+                     log_a = rep(0, n_cell),
+                     Lt = rep(1, n_cell*n_factor-n_factor*(n_factor-1)/2),
+                     x = prop_paths(Y, dt, rep(-2, n_cell), rep(0, n_cell)))
+  data <- list(model="factor_model", n_factor=n_factor, dt=dt, Y=Y, lam=1)
+  adfun <- TMB::MakeADFun(data=data,
+                          parameters=init_param,
+                          random = "x",
+                          DLL = "mnfa_TMBExports", 
+                          silent = T)
+  mod_fit <- nlminb(adfun$par, adfun$fn, adfun$gr) # optimization
+  rep <- TMB::sdreport(adfun) # get Hessian
+  expect_equal(mod_fit$convergence, 0) # Expect convergence indicator is 0, i.e., nlminb has converged
+  expect_true(rep$pdHess) # Expect the Hessian matrix is positive definite, i.e., no NA standard errors
+})
