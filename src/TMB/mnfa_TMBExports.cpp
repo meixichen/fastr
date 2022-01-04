f0f278e40ef91241da7a76a5b3a2e7e25a38a176
meixichen <meixichen2010@gmail.com>
2022-01-04 11:50:35 -0500

    Initialize parallelization support.

--- src/TMB/mnfa_TMBExports.cpp
+++ src/TMB/mnfa_TMBExports.cpp
@@ -3,6 +3,7 @@
 #define TMB_LIB_INIT R_init_mnfa_TMBExports
 #include <TMB.hpp>
 #include "factor_model_big.hpp"
+#include "factor_model_parallel.hpp"
 #include "factor_model.hpp"
 
 template<class Type>
@@ -10,6 +11,8 @@ Type objective_function<Type>::operator() () {
   DATA_STRING(model);
   if(model == "factor_model_big") {
     return factor_model_big(this);
+  } else if(model == "factor_model_parallel") {
+    return factor_model_parallel(this);
   } else if(model == "factor_model") {
     return factor_model(this);
   } else {
