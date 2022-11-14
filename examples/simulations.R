c6b5f764f4ae286debd36c32e9c6a02be75df2e1
m372chen <meixichen2010@gmail.com>
2022-11-14 15:31:48 -0500

    Modify sim plots

--- examples/simulations.R
+++ examples/simulations.R
@@ -99,7 +99,7 @@ lmat_2 <- fit_2step$lmat_unnorm_hat
 
 a_se_2 <- fit_2step$loga_se
 k_se_2 <- fit_2step$logk_se
-ell_se_2 <- diag(fit_2step$lmat_unnorm_cov)
+ell_se_2 <- sqrt(diag(fit_2step$lmat_unnorm_cov))
 
 log_a_j <- fit_joint$log_a_hat
 log_k_j <- fit_joint$log_k_hat
@@ -108,20 +108,26 @@ lmat_j <- fit_joint$lmat_unnorm_hat
 
 a_se_j <- fit_joint$loga_se
 k_se_j <- fit_joint$logk_se
-ell_se_j <- diag(fit_joint$lmat_unnorm_cov)
+ell_se_j <- sqrt(diag(fit_joint$lmat_unnorm_cov))
 ell_se_diff <- ell_se_2 - ell_se_j
+ell_se_ratio <- ell_se_2/ell_se_j
 pdf(paste0(save_path, "2step-vs-joint.pdf"), height=7, width=7)
-par(mfrow=c(2,2), mar=c(4,5,3,1))
+par(mfrow=c(2,2), mar=c(4.5,5.2,3,1))
 cex_lab<-1.5
 cex_main<-2
-plot(log_a_j, log_a_2, cex.lab=cex_lab, cex.main=cex_main, xlab=expression(hat(alpha)["joint"]), 
-     ylab=expression(hat(alpha)["2step"]), main=expression((a)~hat(alpha)))
+plot(log_a_j, log_a_2, cex.lab=cex_lab, cex.main=cex_main, 
+     xlab=expression(widehat(log(alpha))["joint"]), 
+     ylab=expression(widehat(log(alpha))["2step"]), 
+     main=expression((a)~widehat(log(alpha))))
 abline(0,1,lty="dashed", col="blue")
-plot(log_k_j, log_k_2, cex.lab=cex_lab, cex.main=cex_main, xlab=expression(hat(k)["joint"]), 
-     ylab=expression(hat(k)["2step"]), main=expression((b)~hat(k)))
+plot(log_k_j, log_k_2, cex.lab=cex_lab, cex.main=cex_main, 
+     xlab=expression(widehat(log(k))["joint"]), 
+     ylab=expression(widehat(log(k))["2step"]), 
+     main=expression((b)~widehat(log(k))))
 abline(0,1,lty="dashed", col="blue")
 plot(rate_j, rate_2, cex.lab=cex_lab, cex.main=cex_main, xlab=expression(widehat("rate")["joint"]), 
-     ylab=expression(widehat("rate")["2step"]), main=expression((c)~widehat("rate")))
+     ylab=expression(widehat("rate")["2step"]), 
+     main=expression((c)~widehat("rate")~"="~widehat(alpha)~"/"~widehat(k)))
 abline(0,1,lty="dashed", col="blue")
 plot(as.vector(lmat_j), as.vector(lmat_2), cex.lab=cex_lab, cex.main=cex_main,
      xlab=expression(widehat(ell)["joint"]), 
@@ -131,21 +137,23 @@ abline(0,-1,lty="dashed", col="blue")
 dev.off()
 
 pdf(paste0(save_path, "2step-vs-joint-ell-se.pdf"), height=4, width=12)
-par(mfrow=c(1,3), mar=c(4,5,3,1))
+par(mfrow=c(1,3), mar=c(4.5,5.5,3,3))
 cex_axis <- 1.4
-plot(ell_se_j, ell_se_2, xlim=c(0,60), ylim=c(0,60),
+cex_lab <- 1.8
+cex_main <- 2
+plot(ell_se_j, ell_se_2, xlim=c(0,8), ylim=c(0,8),
      cex.lab=cex_lab, cex.main=cex_main, cex.axis=cex_axis,
      xlab=expression("SE"(widehat(ell))["joint"]), 
      ylab=expression("SE"(widehat(ell))["2step"]), 
      main=expression("SE"(widehat(ell))))
-abline(0,1, lty="dashed")
-boxplot(ell_se_diff, cex.lab=cex_lab, cex.main=1.6, cex.axis=cex_axis,
-	main=expression("SE"(widehat(ell))["2step"]-"SE"(widehat(ell))["joint"]), 
+abline(0,1, lty="dashed", col="blue")
+boxplot(ell_se_ratio, cex.lab=cex_lab, cex.main=cex_main, cex.axis=cex_axis,
+	main=expression("SE"(widehat(ell))["2step"]~"/"~"SE"(widehat(ell))["joint"]), 
 	ylab="Difference")
-boxplot(ell_se_diff, cex.lab=cex_lab, cex.main=1.6, cex.axis=cex_axis,
-	main=expression("SE"(widehat(ell))["2step"]-"SE"(widehat(ell))["joint"]("zoomed in")),
+boxplot(ell_se_ratio, cex.lab=cex_lab, cex.main=cex_main, cex.axis=cex_axis,
+	main=expression("SE"(widehat(ell))["2step"]~"/"~"SE"(widehat(ell))["joint"]("zoomed in")),
 	ylab="Difference",
-        ylim=c(-0.3,1.2))
+        ylim=c(0.95,1.4))
 dev.off()
 
 ####### Accuracy check #############
